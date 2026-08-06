c55e577a690fdf47487bdf5e5caf525168d4d8ae
alba <alba.fuentes@m47labs.com>
2026-08-06 12:45:11 +0200

    feat: set staleTime and gcTime to 0 for improved data handling in quiz routing

--- src/routes/features/quiz/index.tsx
+++ src/routes/features/quiz/index.tsx
@@ -30,9 +30,11 @@ export const Route = createRoute({
 
     // staleTime fixed the revalidation of the data. The loader function is not executed again on the client, and the data is not fetched again if it does not become stale.
     // staleTime: 1000 * 60 * 5, // 5 minutes
+    staleTime: 0,
 
     // gcTime fixed the garbage collection of the data. The loader function is not executed again on the client, and the data is not fetched again if the user does not navigate away from the page.
     // gcTime: 1000 * 60 * 10, // 10 minutes
+    gcTime: 0,
 
     // page error 404, the page is not found, the component is rendered, and the user can navigate to another page.
     notFoundComponent: () => <NotFound />,
